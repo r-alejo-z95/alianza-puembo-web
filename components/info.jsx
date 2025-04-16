b49e21349d70e37a7a0c1ae2a74923e4bf2a5a2a
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-15 23:56:53 -0500

    Add info component, still in progress

--- components/info.jsx
+++ components/info.jsx
@@ -1,3 +1,39 @@
+import { Button } from "./ui/button";
+import { landingPageBtnStyles } from "@/lib/styles";
+import Image from "next/image";
+
 export default function Info() {
-  return <div>INFO</div>;
+  return (
+    <section className="w-full h-full flex flex-col items-left justify-center text-(--puembo-black) px-13 gap-8 my-4">
+      <p className="text-2xl md:text-5xl lg:text-6xl">
+        Somos una familia con convicciones firmes en Cristo, que comparte su Fe
+        con otros.
+      </p>
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-col gap-4">
+          <p className="font-merriweather text-lg md:text-2xl lg:text-4xl font-bold">
+            Somos una familia con convicciones firmes en Cristo, que comparte su
+            Fe con otros.
+          </p>
+          <p className="text-sm md:text-lg lg:text-xl">
+            Somos una familia con convicciones firmes en Cristo, que comparte su
+            Fe con otros.
+          </p>
+        </div>
+        <Image
+          src="/info-section.jpg"
+          alt="Mujer abrazando a otra"
+          width={3600}
+          height={2403}
+          className="hidden md:block md:w-[360px] lg:w-[480px] rounded-md"
+        />
+      </div>
+      <div className="flex flex-row min-w-screen md:flex-row gap-4 lg:gap-8">
+        <Button className={landingPageBtnStyles}>Noticias</Button>
+        <Button variant="outline" className={landingPageBtnStyles}>
+          Calendario de eventos
+        </Button>
+      </div>
+    </section>
+  );
 }
