8340e12dbed08c0d56fc60f80c6e39599b6075fc
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-16 20:22:28 -0500

    Adjust spacing and alignment in Info component

--- components/info.jsx
+++ components/info.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export default function Info() {
   return (
-    <section className="w-full h-full flex flex-col items-left justify-center text-(--puembo-black) px-13 gap-8 my-4">
+    <section className="w-full h-full flex flex-col items-left justify-center text-(--puembo-black) px-13 gap-8 my-8">
       <p className="text-2xl md:text-5xl lg:text-6xl">
         Somos una familia con convicciones firmes en Cristo, que comparte su Fe
         con otros.
       </p>
       <div className="flex flex-row gap-4">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 justify-end">
           <p className="font-merriweather text-lg md:text-2xl lg:text-4xl font-bold">
             Somos una familia con convicciones firmes en Cristo, que comparte su
             Fe con otros.
