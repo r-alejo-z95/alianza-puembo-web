cd31922401a918b81ec5305cbbba6eeb35e922dc
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-17 00:06:41 -0500

    Add footer with placeholder text

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,3 +1,54 @@
+import Link from "next/link";
+
+const links = [
+  {
+    title: "Features",
+    href: "#",
+  },
+  {
+    title: "Solution",
+    href: "#",
+  },
+  {
+    title: "Customers",
+    href: "#",
+  },
+  {
+    title: "Pricing",
+    href: "#",
+  },
+  {
+    title: "Help",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+];
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-(--puembo-black) py-12">
+      <div className="mx-auto max-w-5xl px-6">
+        <div className="flex flex-wrap justify-between gap-6">
+          <span className="text-white order-last block text-center text-sm md:order-first">
+            © {new Date().getFullYear()} Alianza Puembo, Todos los derechos
+            reservados
+          </span>
+          <div className="order-first flex flex-wrap justify-center gap-6 text-sm md:order-last">
+            {links.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className="text-white hover:text-accent block duration-150"
+              >
+                <span>{link.title}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
