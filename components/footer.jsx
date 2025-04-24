fcbdf396393f129edb1343451955f01156aade26
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-24 10:23:22 -0500

    Change footer text and add isotype

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "./ui/button";
 
 const links = [
@@ -28,16 +29,24 @@ export default function Footer() {
   return (
     <footer className="bg-(--puembo-black) py-8">
       <div className="mx-auto max-w-5xl px-6">
-        <div className="flex flex-wrap justify-between gap-6">
-          <span className="text-white order-last block text-center text-sm md:order-first">
-            © {new Date().getFullYear()} Alianza Puembo
-            <br />
+        <div className="flex flex-wrap items-center justify-between gap-6">
+          <div className="text-white order-last flex flex-col text-center text-sm md:order-first">
+            <div className="flex gap-1 items-center">
+              <Image
+                src="/iso-white.png"
+                alt="Isotipo Puembo"
+                width={300}
+                height={300}
+                className="size-3.5"
+              />
+              <p>Puembo - {new Date().getFullYear()}</p>
+            </div>
             <Link href="https://github.com/r-alejo-z95">
               <Button variant="link" className="text-white text-[10px]">
                 Desarrollado por RZ
               </Button>
             </Link>
-          </span>
+          </div>
           <div className="order-first flex flex-wrap justify-center gap-6 text-sm md:order-last">
             {links.map((link, index) => (
               <Link
