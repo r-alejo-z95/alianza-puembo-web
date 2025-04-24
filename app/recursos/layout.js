0689aeb61977f419a2c817ed1db34ab66c59ba18
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-23 23:31:46 -0500

    Add Recursos layout and Lom page components

---
+++ app/recursos/layout.js
@@ -0,0 +1,3 @@
+export default function RecursosLayout({ children }) {
+  return <section className="">{children}</section>;
+}
---
+++ app/recursos/lom/page.js
@@ -0,0 +1,7 @@
+export default function Lom() {
+  return (
+    <>
+      <h1 className="text-9xl">Lom</h1>
+    </>
+  );
+}
