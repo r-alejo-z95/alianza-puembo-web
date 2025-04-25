fb40c205954863c1184dc34543a8bbe7a46fe7e3
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-04-25 10:15:33 -0500

    Add custom church icon to map

--- components/interactive-map.jsx
+++ components/interactive-map.jsx
@@ -3,16 +3,23 @@
 import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
-export default function MapaUbicacion() {
+export default function InteractiveMap() {
   useEffect(() => {
     import("leaflet").then((L) => {
+      const customIcon = L.icon({
+        iconUrl: "/icons/church-icon.png",
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40],
+      });
+
       const map = L.map("map").setView([-0.1935895, -78.3646483], 17);
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; OpenStreetMap contributors",
       }).addTo(map);
 
-      L.marker([-0.1935895, -78.3646483])
+      L.marker([-0.1935895, -78.3646483], { icon: customIcon })
         .addTo(map)
         .bindPopup(
           "<b>Iglesia Alianza Puembo</b><br>Julio Tobar Donoso y 24 de Mayo"
