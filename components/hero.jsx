0ee677a06600c40405e817944ebf40c9618f9170
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-05-05 22:47:48 -0500

    Refactor Hero component to use utility classes for heading sizes and add new text size constants

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,17 +1,16 @@
 import { Button } from "./ui/button";
-import { landingPageBtnStyles } from "@/lib/styles";
+import { cn } from "@/lib/utils";
+import { h1TextSizes, h2TextSizes, landingPageBtnStyles } from "@/lib/styles";
 
 export default function Hero() {
   return (
     <section className="relative w-full h-screen bg-[url('/Hero.jpg')] bg-cover bg-top bg-no-repeat">
       <div className="w-full h-full flex flex-col justify-center items-start text-white backdrop-brightness-80 backdrop-contrast-70 px-4 md:px-6 lg:px-8 gap-6">
         <div className="flex flex-col gap-2 mb-4">
-          <h2 className="text-base md:text-xl lg:text-2xl">
+          <h2 className={cn(h2TextSizes)}>
             Experimenta la presencia de Dios en casa
           </h2>
-          <h1 className="font-merriweather text-4xl md:text-7xl lg:text-8xl font-bold">
-            Bienvenido
-          </h1>
+          <h1 className={cn(h1TextSizes)}>Bienvenido</h1>
         </div>
         <div className="flex flex-col md:flex-row gap-4">
           <Button className={landingPageBtnStyles}>Visítanos</Button>
