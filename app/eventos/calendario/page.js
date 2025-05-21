2ec22315314de8305233a2c202cf8ed9b66f44c3
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-05-20 23:14:26 -0500

    feat: add admin and user calendar components with event management functionality

--- app/eventos/calendario/page.js
+++ app/eventos/calendario/page.js
@@ -1,7 +1,9 @@
+import UserCalendar from "@/components/UserCalendar";
+
 export default function Calendario() {
   return (
     <>
-      <h1 className="text-9xl">Calendario</h1>
+      <UserCalendar />
     </>
   );
 }
