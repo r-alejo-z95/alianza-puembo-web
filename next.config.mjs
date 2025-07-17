3d97c1c22d5fc78cf28a36a268841163a5fd6d1c
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-07-17 10:05:43 -0500

    feat: enhance Next.js configuration for performance
    - Enable  for optimized deployment output.
    - Activate  for Gzip compression of assets.
    - Ensure  and  are set for development best practices and faster minification.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
