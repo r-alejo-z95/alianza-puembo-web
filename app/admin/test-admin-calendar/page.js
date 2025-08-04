86965928aa41dcdccd983a72296cc30d37ba148d
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-08-04 12:14:17 -0500

    test user calendar

---
+++ app/admin/test-admin-calendar/page.js
@@ -0,0 +1,9 @@
+import { AdminCalendar } from "@/components/shared/CalendarOrigin";
+
+export default function testAdminCalendar() {
+    return (
+        <main className="p-16">
+            <AdminCalendar />
+        </main>
+    )
+}
---
+++ app/test-calendar/page.js
@@ -0,0 +1,9 @@
+import { UserCalendar } from "@/components/shared/CalendarOrigin";
+
+export default function testCalendar() {
+    return (
+        <main className="p-16">
+            <UserCalendar />
+        </main>
+    )
+}
